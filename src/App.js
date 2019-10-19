545b7947b8c624a999f0e10b645da05a8fe021e7
AidenBow <AidenBow@users.noreply.github.com>
2019-10-18 20:15:18 -0700

    fixed bugs with useReducer and put example in reducers.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import ParentOrVolunteer from './components/ParentOrVolunteer';
 
 import { StateProvider } from "./state/state"
-import { reducer } from "./state/reducers"
+import { reducer, initialState } from "./state/reducers"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <StateProvider reducer={reducer}>
+        <StateProvider initialState={initialState} reducer={reducer}>
           <ParentOrVolunteer />
         </StateProvider>
       </header>
--- src/state/reducers.js
+++ src/state/reducers.js
@@ -1,14 +1,24 @@
-const initialState = {
-    user: "testuser"
-};
+import {axiosWithAuth} from "../utils/axiosWithAuth"
 
-export const reducer = (state = initialState, action) => {
-    switch(action.type){
-        case "example" :
-            return (
-                state
-            )
+export const initialState = {
+    user: "testuser",
+    userlist: {}
+  };
+
+export const reducer = (state, action) => {
+    switch(action){
+        case "TEST" :            
+            return {
+                ...state,
+                user: 'new username'
+            }
         default :
             return state
     }
 }
+
+// for importing state,
+// import { useStateValue } from './state';
+// then use 
+// const [{ user, userlist }, dispatch] = useStateValue();
+// example <button onClick={() => dispatch("TEST")}>test</button>
