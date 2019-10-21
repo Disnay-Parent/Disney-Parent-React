3fd2bdeb27c373bd305ce2e57cb4f20595860bbc
lizdoyle <lizdoyle1121@gmail.com>
2019-10-20 21:29:58 -0400

    working on sign up, and nav basics

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,77 @@
-import React from "react";
+import React from 'react'
+import {withFormik, Form, Field, withFormik} from 'formik';
+import * as Yup from 'yup';
+// import {axiosWithAuth} from '../utils/axiosWithAuth';
 
-const Login = () => {
+function Login() {
   return (
-    <div> </div>
+    <div>
+      <Form>
+        <div>
+          Are you a Parent or Volunteer?
+          <label>Parent</label>
+          <Field 
+            name="parentOrVolunteer"
+            type="radio"
+            value="parent"
+          />
+          <label>Volunteer</label>
+          <Field
+            name="parentOrVolunteer"
+            type="radio"
+            value="volunteer"
+          />
+        </div>
+        <div>
+          <label>Username:</label>
+          <Field 
+            name="username"
+            type="text"
+            placeholder="username"
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <Field
+            name="password"
+            type="password"
+            placeholder="password"
+          />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </Form>
+    </div>
   );
-};
+}
 
-export default Login;
+const LoginFormikForm = withFormik({
+  mapPropsToValues({parentOrVolunteer, username, password}) {
+    return {
+      parentOrVolunteer: parentOrVolunteer || "",
+      username: username || "",
+      password: password || ""
+    };
+  },
+
+  // Validation Schema
+  validationSchema: Yup.object().shape({
+    parentOrVolunteer: Yup.string()
+      .required("Must select either Parent or Volunteer"),
+    username: Yup.string()
+      .min(6, "Username must have at least 6 characters")
+      .required("Username is required"),
+    password: Yup.string()
+      .min(6, "Password must have at least 6 characters")
+      .required("Password is reuired"),
+  }),
+  // End Validation Schema
+
+  handleSubmit() {
+      // Need React II's help here
+    },
+  
+})(Login)
+
+export default LoginFormikForm;
