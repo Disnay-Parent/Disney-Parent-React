fc7112c1212c42325466d3f04d746c82784faf9f
lizdoyle <lizdoyle1121@gmail.com>
2019-10-23 19:09:35 -0400

    revertedback to Aidens working signup with Formik

--- src/components/SignUp/VolunteerSignUpForm.js
+++ src/components/SignUp/VolunteerSignUpForm.js
@@ -1,147 +1,99 @@
-import React, {useState} from 'react';
-// import {withFormik, Form, Field} from 'formik';
-// import * as Yup from 'yup';
+import React from 'react';
+import {withFormik, Form, Field} from 'formik';
+import * as Yup from 'yup';
 import {axiosWithAuth} from "../../utils/axiosWithAuth";
 
-function VolunteerSignUpForm(props) {
+function VolunteerSignUpForm({errors, touched}, props) {
 
-  const [credentials, setCredentials] = useState({});
-
-    
-  const volunteerSignUp = e => {
-     e.preventDefault();
-     axiosWithAuth().post('/auth/register', credentials)
-             .then(res => {
-                 console.log(res.data)
-
-                 localStorage.setItem('token', res.data.token);
-                 props.history.location.push('/');
-             })
-             .catch(err => console.log(err));
- }
-
- const signUpChangeHandler = event => {
-
-     setCredentials({
-         ...credentials,
-          [event.target.name]: event.target.value 
-         });
- }
+ 
 
   return (
     <div>
       <h1>Volunteer Sign Up Form</h1>
-      <form onSubmit={volunteerSignUp}>
-      <div>
-      <label>Account Type:</label>
-        <input 
-              name="type"
-              type="string"
-              placeholder="volunteer"
-              value={credentials.type}
-              onChange={signUpChangeHandler}
-            />
-        </div>
+      <Form>
         <div>
           <label>Username:</label>
-          {/* {touched.username && errors.username && <p>{errors.username}</p>} */}
-            <input 
+          {touched.username && errors.username && <p>{errors.username}</p>}
+            <Field 
               name="username"
               type="text"
               placeholder="username"
-              value={credentials.username}
-              onChange={signUpChangeHandler}
-            />
-        </div>
-        <div>
-          <label>Password:</label>
-          {/* {touched.password && errors.password && <p>{errors.password}</p>} */}
-            <input  
-              name="password"
-              type="password"
-              placeholder="password"
-              value={credentials.password}
-              onChange={signUpChangeHandler}
-            />
-        </div>
-        <div>
-          <label>First Name:</label>
-          {/* {touched.firstName && errors.firstName && <p>{errors.firstName}</p>} */}
-            <input 
-              name="firstName"
-              type="text"
-              placeholder="First Name"
-              value={credentials.firstName}
-              onChange={signUpChangeHandler}
-            />
-        </div>
-        <div>
-          <label>Last Name:</label>
-          {/* {touched.lastName && errors.lastName && <p>{errors.lastName}</p>} */}
-            <input 
-              name="lastName"
-              type="text"
-              placeholder="Last Name"
-              value={credentials.lastName}
-              onChange={signUpChangeHandler}
             />
         </div>
         <div>
           <label>Email:</label>
-          {/* {touched.email && errors.email && <p>{errors.email}</p>} */}
-            <input 
+          {touched.email && errors.email && <p>{errors.email}</p>}
+            <Field 
               name="email"
               type="email"
               placeholder="email"
-              value={credentials.email}
-              onChange={signUpChangeHandler}
             />
         </div>
-        
         <div>
-          <label>Date of Birth:</label>
-          {/* {touched.DOB && errors.DOB && <p>{errors.DOB}</p>} */}
-            <input 
+          <label>Password:</label>
+          {touched.password && errors.password && <p>{errors.password}</p>}
+            <Field  
+              name="password"
+              type="password"
+              placeholder="password"
+            />
+        </div>
+        <div>
+          <label>First Name:</label>
+          {touched.firstName && errors.firstName && <p>{errors.firstName}</p>}
+            <Field 
+              name="firstName"
+              type="text"
+              placeholder="First Name"
+            />
+        </div>
+        <div>
+          <label>Last Name:</label>
+          {touched.lastName && errors.lastName && <p>{errors.lastName}</p>}
+            <Field 
+              name="lastName"
+              type="text"
+              placeholder="Last Name"
+            />
+        </div>
+        <div>
+          <label>DOB:</label>
+          {touched.DOB && errors.DOB && <p>{errors.DOB}</p>}
+            <Field 
               name="DOB"
               type="date"
-              placeholder="dob"
-              value={credentials.DOB}
-              onChange={signUpChangeHandler}
+              placeholder="Age"
             />
         </div>
         <div>
           <label>Phone Number:</label>
-          {/* {touched.phoneNum && errors.phoneNum && <p>{errors.phoneNum}</p>} */}
-            <input 
+          {touched.phoneNum && errors.phoneNum && <p>{errors.phoneNum}</p>}
+            <Field 
               name="phoneNum"
               type="text"
               placeholder="phone number"
-              value={credentials.phoneNum}
-              onChange={signUpChangeHandler}
             />
         </div>
         <div>
           <label>Average Cost Per Child:</label>
-          {/* {touched.avgPerChild && errors.avgPerChild && <p>{errors.avgPerChild}</p>} */}
-            <input 
+          {touched.avgPerChild && errors.avgPerChild && <p>{errors.avgPerChild}</p>}
+            <Field 
               name="avgPerChild"
-              type="number"
+              type="text"
               placeholder="amount"
-              value={"0" || credentials.avgPerChild}
-              onChange={signUpChangeHandler}
             />
           </div>
           <div>
             <label>Are you willing to negotiate the cost of service?</label>
-            {/* {touched.negotiable && errors.negotiable && <p>{errors.negotiable}</p>} */}
-            <input 
+            {touched.negotiable && errors.negotiable && <p>{errors.negotiable}</p>}
+            <Field 
               name="negotiable"
               type="radio"
               placeholder="amount"
               value={true}
             />
             Yes
-            <input
+            <Field
               name="negotiable"
               type="radio"
               placeholder="amount"
@@ -151,14 +103,14 @@ function VolunteerSignUpForm(props) {
           </div>
           <div>
             <label>CPR Certified</label>
-            {/* {touched.cprCertified && errors.cprCertified && <p>{errors.cprCertified}</p>} */}
-            <input  
+            {touched.cprCertified && errors.cprCertified && <p>{errors.cprCertified}</p>}
+            <Field  
               name="cprCertified"
               type="radio"
-              value=""
+              value="yes"
             />
             Yes
-            <input
+            <Field
               name="cprCertified"
               type="radio"
               value="no"
@@ -168,81 +120,84 @@ function VolunteerSignUpForm(props) {
         <div>
           <button type="submit">Sign Up</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
 }
 
-// const VolunteerFormikSignUp = withFormik({
-//   mapPropsToValues({username, email, password, name, 
-//                     lastName, DOB, phoneNum, avgPerChild,
-//                     negotiable, cprCertified}) {
-//     return {
-//       username: username || "",
-//       email: email || "",
-//       password: password || "",
-//       name: name || "",
-//       lastName: lastName || "",
-//       DOB: DOB || "",
-//       phoneNum: phoneNum || "",
-//       avgPerChild: avgPerChild || "",
-//       negotiable: negotiable || "No",
-//       cprCertified: cprCertified || "No"
-//     };
-//   },
+ const dash = props => {
+   props.history.push("/dashboard");
+ };
 
-//   // Validation Schema
-//   validationSchema: Yup.object().shape({
-//     username: Yup.string()
-//       .min(6, "Username must have at least 6 characters")
-//       .required("Username is required"),
-//     email: Yup.string()
-//       .email("Email is not valid")
-//       .required("Email is required"),
-//     password: Yup.string()
-//       .min(6, "Password must have at least 6 characters")
-//       .required("Password is reuired"),
-//     name: Yup.string()
-//       .min(3, "First name must have at least 3 characters")
-//       .required("First name is required"),
-//     lastName: Yup.string()
-//       .min(3, "Last name must have at least 3 characters")
-//       .required("Last name is required"),
-//     age: Yup.number()
-//       .positive("Age must be a positive number")
-//       .integer("Age must be a whole number")
-//       .min(18, "Must be 18 or older")
-//       .required("Age is required"),
-//     phoneNum: Yup.number()
-//       .min(10, "Phone number must be 10 digits long")
-//       .positive("Phone number must be a positive number")
-//       .required("Phone number is reuqired"),
-//     avgPerChild: Yup.number()
-//       .positive("Average price must be a positive number")
-//       .required("Average price is reuqired"),
-//     negotiable: Yup.string()
-//       .required("Must select 'Yes' or 'No'"),
-//     cprCertified: Yup.string()
-//       .required("Must select 'Yes' or 'No'"),
-//   }),
+const VolunteerFormikSignUp = withFormik({
+  mapPropsToValues({username, email, password, firstName, 
+                    lastName, DOB, phoneNum, avgPerChild,
+                    negotiable, cprCertified}) {
+    return {
+      username: username || "",
+      email: email || "",
+      password: password || "",
+      firstName: firstName || "",
+      lastName: lastName || "",
+      DOB: DOB || "",
+      phoneNum: phoneNum || "",
+      avgPerChild: avgPerChild || "",
+      negotiable: negotiable || "No",
+      cprCertified: cprCertified || "No"
+    };
+  },
+
+  // Validation Schema
+  validationSchema: Yup.object().shape({
+    username: Yup.string()
+      .min(6, "Username must have at least 6 characters")
+      .required("Username is required"),
+    email: Yup.string()
+      .email("Email is not valid")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password must have at least 6 characters")
+      .required("Password is reuired"),
+    firstName: Yup.string()
+      .min(3, "First name must have at least 3 characters")
+      .required("First name is required"),
+    lastName: Yup.string()
+      .min(3, "Last name must have at least 3 characters")
+      .required("Last name is required"),
+    DOB: Yup.date()
+      .required("Age is required"),
+    phoneNum: Yup.number()
+      .min(10, "Phone number must be 10 digits long")
+      .positive("Phone number must be a positive number")
+      .required("Phone number is reuqired"),
+    avgPerChild: Yup.number()
+      .positive("Average price must be a positive number")
+      .required("Average price is reuqired"),
+    negotiable: Yup.string()
+      .required("Must select 'Yes' or 'No'"),
+    cprCertified: Yup.string()
+      .required("Must select 'Yes' or 'No'")
+  }),
   // End Validation Schema
 
-  // handleSubmit(values, {resetForm, setErrors, setSubmitting}) {
-  //   if (values.email === "tiffanyfeldkamp@gmail.com") {
-  //     setErrors({email: "That email is already taken"});
-  //   } else {
-  //     axiosWithAuth()
-  //       .post('/auth/register', values)
-  //       .then(res => {
-  //         console.log(res);
-  //         resetForm();
-  //         setSubmitting(true);
-  //       })
-  //       .catch(err => console.log(err));
-  //       setSubmitting(false);
-  //   }
-  // }
+  handleSubmit(values, {resetForm, setErrors, setSubmitting}) {
+    if (values.email === "tiffanyfeldkamp@gmail.com") {
+      setErrors({email: "That email is already taken"});
+    } else {
+      console.log({ ...values, type: "volunteer" });
+      axiosWithAuth()
+        .post("/auth/register", { ...values, type: "volunteer" })
+        .then(res => {
+          console.log(res);
+          setSubmitting(true);
+          localStorage.setItem("token", res.data.token);
+          dash();
+        })
+        .catch(err => console.log(err));
+        setSubmitting(false);
+    }
+  }
 
-// })(VolunteerSignUpForm);
+})(VolunteerSignUpForm);
 
-export default VolunteerSignUpForm;
+export default VolunteerFormikSignUp;
