ff97c9b2ec98ac21e9d6ec4f2bf355cb2c0c4058
AidenBow <AidenBow@users.noreply.github.com>
2019-10-23 23:21:26 -0700

    added axios calls to comment section

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -4,9 +4,9 @@ const Comment = (props) => {
 
     return (
       <div>
-        <p>{props.name}</p>
+        <p>{props.name}</p> 
         <p>{props.comment}</p>
       </div>
-    );
+    ); //need api documentation, just guessing what the props are
 }
 export default Comment
--- src/components/Comments/CommentSection.js
+++ src/components/Comments/CommentSection.js
@@ -1,12 +1,26 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import Comment from "./Comment"
+import CommentForm from "./CommentForm"
+import {axiosWithAuth} from "../../utils/axiosWithAuth"
 
-const CommentSection = () => {
+const CommentSection = (props) => {
 
+    const [comments, setComments] = useState([])
+    const post_id = props.id
+
+    useEffect(() => {
+        axiosWithAuth
+            .get(`/comments/${post_id}`) //need api documentation
+            .then(res => {
+                console.log(res)
+                setComments(res)
+            })
+            .catch(err => console.log(err))
+    }, [])
 
     return(
         <div>
-            {Comments.map(comment => {
+            {comments.map(comment => {
                 return <Comment comment={comment}/>
             })}
             <CommentForm />
